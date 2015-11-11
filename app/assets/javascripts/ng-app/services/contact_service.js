e7330b16add986a247c4bb0660e37ea815499b5e
jquigley1003 <jeff.quigley@wavinghi.com>
2015-11-11 13:36:20 -0500

    update the contact service for production

--- app/assets/javascripts/ng-app/services/contact_service.js
+++ app/assets/javascripts/ng-app/services/contact_service.js
@@ -1,6 +1,6 @@
 angular.module('fxChiropracticApp')
   .service('contactService',['$resource', function($resource) {
-  return $resource("http://localhost:3000/contacts/:id", { id: '@id' },
+  return $resource('/contacts/:id', { id: '@id' },
     {
       'new':  { method: 'POST' },      
       'create':  { method: 'POST' }
